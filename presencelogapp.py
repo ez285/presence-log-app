0a73f523cd79afdbeecee9e0668b65c0fc23a4a6
ez285 <ezervas.zemcogroup@gmail.com>
2026-02-10 01:09:04 +0200

    Update presencelogapp.py

--- presencelogapp.py
+++ presencelogapp.py
@@ -14,22 +14,32 @@ def get_worksheet():
     creds = Credentials.from_service_account_info(sl.secrets['gcp_service_account'], scopes=SCOPES)
     gc = gs.authorize(creds)
     sh = gc.open_by_key(sl.secrets['sheet_id'])
-    return sh.worksheet(sl.secrets['worksheet_name'])
+    return sh.worksheet(sl.secrets['worksheet_name_daily']), sh.worksheet(sl.secrets['worksheet_name_list'])
 
 class PresenceLog:
     def __init__(self) -> None:
-        self.Sheet = get_worksheet()
+        self.SheetDaily, self.SheetList = get_worksheet()
+        self.PersonellList = self.SheetList.get_all_records()
+        self.Companies = list(set(rec['Contractor'] for rec in self.PersonellList))
         
     def AddRows(self, rows:list[list]) -> None:
-        self.Sheet.append_rows(rows, insert_data_option=gs.utils.InsertDataOption.insert_rows, table_range='A1')
+        self.SheetDaily.append_rows(rows, insert_data_option=gs.utils.InsertDataOption.insert_rows, table_range='A1')
+    
+    def GetPersonellForCompany(self, company:str) -> list:
+        return [[rec['Local ID'], rec['First Name'], rec['Last Name']] for rec in self.PersonellList if rec['Contractor'] == company]
+
 
 pl = PresenceLog()
-pl.AddRows([['10/02/2026', 'KAMERIS', 'Mitsaras', 'Arxontidis']])
 
 with sl.form('presence log'):
-    selected_date = sl.date_input('Date', value=date.today())
-    note = sl.text_input('Note (for now)')
-    submitted = sl.form_submit_button("Save")
-if submitted:
+    selDat = sl.date_input('Date', value=date.today(), format='DD/MM/YYYY')
+    cont = sl.selectbox('Company', pl.Companies + ['Add New...'])
+    if cont == 'Add New...':
+        pass
+    elif cont is not None:
+        selPres = sl.multiselect('Presence', pl.GetPersonellForCompany(str(cont)) + ['Add New...'])
 
+    submitted = sl.form_submit_button("Save")
+
+if submitted:
     sl.success('Saved')
